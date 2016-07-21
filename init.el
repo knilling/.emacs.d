36aca777dccb93ba5fac44bafc6b28a65587ade0
Chris Crawford <christopher.p.crawford@gmail.com>
2016-07-21 19:52:51 -0400

    Load JSShell via init.el
    Load JSShell and define a few custom key bindings for Javascript mode that allow me to send chunks of JavaScript to the JSShell console.

--- init.el
+++ init.el
@@ -10,3 +10,14 @@
                          ("melpa" . "https://melpa.org/packages/")))
 
 (package-initialize)
+
+;; Add jsshell to the load path
+(add-to-list 'load-path "~/.emacs.d/jsshell/")
+
+;; JSShell
+(require 'jsshell-bundle)
+;;; Borrowed this technique from http://superuser.com/a/684404
+(with-eval-after-load 'js
+  (define-key js-mode-map "\C-x\C-e" 'jsshell-send-last-sexp)
+  (define-key js-mode-map "\C-c\C-e" 'jsshell-send-region))
+

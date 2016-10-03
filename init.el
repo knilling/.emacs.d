919a6e33488fb71bba71c84cb2a6c7b53b916d3c
Chris Crawford <christopher.p.crawford@gmail.com>
2016-10-03 16:31:15 -0400

    Add paredit

--- init.el
+++ init.el
@@ -52,6 +52,16 @@
 
 (require 'restclient)
 
+;; paredit
+(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+(add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+(add-hook 'clojure-mode-hook 'enable-paredit-mode)
+
+
 ;; Borrowing these from https://github.com/flyingmachine/emacs-for-clojure
 ;; These customizations make editing a bit nicer.
 (load "editing.el")

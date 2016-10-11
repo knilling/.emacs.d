92dc5104b49720e202cdc70467a87cd5d029420f
Chris Crawford <christopher.p.crawford@gmail.com>
2016-10-11 14:35:43 -0400

    Add url encode/decode region funtions

--- init.el
+++ init.el
@@ -112,3 +112,18 @@
 ;; load theme
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (load-theme 'cyberpunk t)
+
+; From http://www.blogbyben.com/2010/08/handy-emacs-function-url-decode-region.html
+(defun url-decode-region (start end)
+  "Replace a region with the same contents, only URL decoded."
+  (interactive "r")
+  (let ((text (url-unhex-string (buffer-substring start end))))
+    (delete-region start end)
+    (insert text)))
+
+(defun url-encode-region (start end)
+  "Replace a region with the same contents, only URL encoded."
+  (interactive "r")
+  (let ((text (url-hexify-string (buffer-substring start end))))
+    (delete-region start end)
+    (insert text)))
